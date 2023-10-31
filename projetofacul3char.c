c19dc563a8fc1607b97ce2eb032e32d859e581b3
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2023-10-31 16:37:38 -0300

    projetofacul3char

---
+++ projetofacul3char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+
+    int quant;
+    float peso;
+    char letra;
+
+    quant = 10;
+    peso = 80.5;
+    letra = 'a';
+
+    printf("%i\n", quant);
+    printf("%f\n", peso);
+    printf("%.2f\n", peso);
+    printf("%c\n", letra);
+
+return 0;
+}
